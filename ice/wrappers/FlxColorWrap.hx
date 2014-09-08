aeaea4a9f55addc59a56138484397a6930213693
NicoM1 <nico.may99@gmail.com>
2014-09-08 12:14:02 -0700

    add color wrapper

---
+++ ice/wrappers/FlxColorWrap.hx
@@ -0,0 +1,23 @@
+package ;
+
+import flixel.util.FlxColor;
+
+class FlxColorWrap
+{
+	public static var TRANSPARENT:FlxColor =   0x00000000;
+	public static var WHITE:FlxColor =         0xFFFFFFFF;
+	public static var GRAY:FlxColor =          0xFF808080;
+	public static var BLACK:FlxColor =         0xFF000000;
+	
+	public static var GREEN:FlxColor =         0xFF008000;
+	public static var LIME:FlxColor =          0xFF00FF00;
+	public static var YELLOW:FlxColor =        0xFFFFFF00;
+	public static var ORANGE:FlxColor =        0xFFFFA500;
+	public static var RED:FlxColor =           0xFFFF0000;
+	public static var PURPLE:FlxColor =        0xFF800080;
+	public static var BLUE:FlxColor =          0xFF0000FF;
+	public static var BROWN:FlxColor =         0xFF8B4513;
+	public static var PINK:FlxColor =          0xFFFFC0CB;
+	public static var MAGENTA:FlxColor =       0xFFFF00FF;
+	public static var CYAN:FlxColor =          0xFF00FFFF;	
+}
