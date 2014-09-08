215fef1b1c6423d986ec0b82a169a956fa785e04
NicoM1 <nico.may99@gmail.com>
2014-09-08 12:07:39 -0700

    Add FlxKeyWrap

---
+++ ice/wrappers/FlxKeyWrap.hx
@@ -0,0 +1,111 @@
+package ice.wrappers;
+
+import flixel.input.keyboard.FlxKey;
+
+class FlxKeyWrap
+{
+	
+	/*public static var LEFT(get,null):FlxKey;
+	
+	private static inline function get_LEFT():FlxKey
+	{
+		return FlxKey.LEFT;
+	}*/
+	
+	public static var ANY           (default, never)  = -2;
+	public static var NONE          (default, never)  = -1;
+	public static var A             (default, never)  = 65;
+	public static var B             (default, never)  = 66;
+	public static var C             (default, never)  = 67;
+	public static var D             (default, never)  = 68;
+	public static var E             (default, never)  = 69;
+	public static var F             (default, never)  = 70;
+	public static var G             (default, never)  = 71;
+	public static var H             (default, never)  = 72;
+	public static var I             (default, never)  = 73;
+	public static var J             (default, never)  = 74;
+	public static var K             (default, never)  = 75;
+	public static var L             (default, never)  = 76;
+	public static var M             (default, never)  = 77;
+	public static var N             (default, never)  = 78;
+	public static var O             (default, never)  = 79;
+	public static var P             (default, never)  = 80;
+	public static var Q             (default, never)  = 81;
+	public static var R             (default, never)  = 82;
+	public static var S             (default, never)  = 83;
+	public static var T             (default, never)  = 84;
+	public static var U             (default, never)  = 85;
+	public static var V             (default, never)  = 86;
+	public static var W             (default, never)  = 87;
+	public static var X             (default, never)  = 88;
+	public static var Y             (default, never)  = 89;
+	public static var Z             (default, never)  = 90;
+	public static var ZERO          (default, never)  = 48;
+	public static var ONE           (default, never)  = 49;
+	public static var TWO           (default, never)  = 50;
+	public static var THREE         (default, never)  = 51;
+	public static var FOUR          (default, never)  = 52;
+	public static var FIVE          (default, never)  = 53;
+	public static var SIX           (default, never)  = 54;
+	public static var SEVEN         (default, never)  = 55;
+	public static var EIGHT         (default, never)  = 56;
+	public static var NINE          (default, never)  = 57;
+	public static var PAGEUP        (default, never)  = 33;
+	public static var PAGEDOWN      (default, never)  = 34;
+	public static var HOME          (default, never)  = 36;
+	public static var END           (default, never)  = 35;
+	public static var INSERT        (default, never)  = 45;
+	public static var ESCAPE        (default, never)  = 27;
+	public static var MINUS         (default, never)  = 189;
+	public static var PLUS          (default, never)  = 187;
+	public static var DELETE        (default, never)  = 46;
+	public static var BACKSPACE     (default, never)  = 8;
+	public static var LBRACKET      (default, never)  = 219;
+	public static var RBRACKET      (default, never)  = 221;
+	public static var BACKSLASH     (default, never)  = 220;
+	public static var CAPSLOCK      (default, never)  = 20;
+	public static var SEMICOLON     (default, never)  = 186;
+	public static var QUOTE         (default, never)  = 222;
+	public static var ENTER         (default, never)  = 13;
+	public static var SHIFT         (default, never)  = 16;
+	public static var COMMA         (default, never)  = 188;
+	public static var PERIOD        (default, never)  = 190;
+	public static var SLASH         (default, never)  = 191;
+	public static var NUMPADSLASH   (default, never)  = 191;
+	public static var GRAVEACCENT   (default, never)  = 192;
+	public static var CONTROL       (default, never)  = 17;
+	public static var ALT           (default, never)  = 18;
+	public static var SPACE         (default, never)  = 32;
+	public static var UP            (default, never)  = 38;
+	public static var DOWN          (default, never)  = 40;
+	public static var LEFT          (default, never)  = 37;
+	public static var RIGHT         (default, never)  = 39;
+	public static var TAB           (default, never)  = 9;
+	public static var PRINTSCREEN   (default, never)  = 301;
+	public static var F1            (default, never)  = 112;
+	public static var F2            (default, never)  = 113;
+	public static var F3            (default, never)  = 114;
+	public static var F4            (default, never)  = 115;
+	public static var F5            (default, never)  = 116;
+	public static var F6            (default, never)  = 117;
+	public static var F7            (default, never)  = 118;
+	public static var F8            (default, never)  = 119;
+	public static var F9            (default, never)  = 120;
+	public static var F10           (default, never)  = 121;
+	public static var F11           (default, never)  = 122;
+	public static var F12           (default, never)  = 123;
+	public static var NUMPADZERO    (default, never)  = 96;
+	public static var NUMPADONE     (default, never)  = 97;
+	public static var NUMPADTWO     (default, never)  = 98;
+	public static var NUMPADTHREE   (default, never)  = 99;
+	public static var NUMPADFOUR    (default, never)  = 100;
+	public static var NUMPADFIVE    (default, never)  = 101;
+	public static var NUMPADSIX     (default, never)  = 102;
+	public static var NUMPADSEVEN   (default, never)  = 103;
+	public static var NUMPADEIGHT   (default, never)  = 104;
+	public static var NUMPADNINE    (default, never)  = 105;
+	public static var NUMPADMINUS   (default, never)  = 109;
+	public static var NUMPADPLUS    (default, never)  = 107;
+	public static var NUMPADPERIOD  (default, never)  = 110;
+	public static var NUMPADMULTIPLY(default, never)  = 106;
+}
